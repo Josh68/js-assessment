aab7b456358bafab49d4defd9adc57368f636d46
Josh Schneider <josh68@gmail.com>
2016-10-13 21:59:00 -0700

    new tests

--- app/count.js
+++ app/count.js
@@ -2,6 +2,15 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    var i = start;
+    var intervalFunc = function () {
+      if (i <= end) {
+        console.log(i);
+        i++;
+      } else {
+        clearInterval(intervalFunc);
+      }
+    };
+    setInterval(intervalFunc, 100);
   }
 };
