e2114fc9fd44b262af58a8932699294f997a0bbf
Josh Schneider <josh68@gmail.com>
2016-10-13 22:44:59 -0700

    finished the count tests after some confusion

--- app/count.js
+++ app/count.js
@@ -3,14 +3,20 @@ exports = typeof window === 'undefined' ? global : window;
 exports.countAnswers = {
   count: function (start, end) {
     var i = start;
-    var intervalFunc = function () {
+    console.log(i);
+    i++;
+    var intervalVar = setInterval(intervalFunc.bind(this), 100);
+    function intervalFunc () {
       if (i <= end) {
         console.log(i);
         i++;
-      } else {
-        clearInterval(intervalFunc);
       }
     };
-    setInterval(intervalFunc, 100);
+    function cancel () {
+      clearInterval(intervalVar);
+    }
+    return {
+      cancel: cancel
+    };
   }
 };
