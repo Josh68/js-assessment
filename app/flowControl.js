a00405a5b24cc18ac8fc9b6ac0ca6f25723c51f4
Josh Schneider <josh68@gmail.com>
2016-10-16 23:24:58 -0700

    some functions tests

--- app/flowControl.js
+++ app/flowControl.js
@@ -2,6 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.flowControlAnswers = {
   fizzBuzz: function(num) {
+    function evDiv(_num, div) {
+        return _num % div === 0;
+    }
+    
+    if (num && typeof num === 'number') {
+        const isDiv3 = evDiv(num, 3);
+        const isDiv5 = evDiv(num, 5);
+        return isDiv3 && !isDiv5 ? 'fizz' : isDiv5 && !isDiv3 ? 'buzz' : isDiv3 && isDiv5 ? 'fizzbuzz' : num;
+    }
+
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
@@ -10,6 +20,6 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
-
+    return false;
   }
 };
--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    //return fn.apply(null, arr);
+    return fn(...arr);
   },
 
   speak: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction: function(str) {
-
+    return function(otherString) {
+      return str + ', ' + otherString;
+    }
   },
 
   makeClosures: function(arr, fn) {
-
+    var ret = [];
+    for (let i=0; i<arr.length; i++) {
+      ret.push( function() {
+        return fn(arr[i]);
+      });
+    }
+    return ret;
   },
 
   partial: function(fn, str1, str2) {
