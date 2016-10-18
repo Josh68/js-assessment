250510bab9c2e98c8869533e3bd67c55c6dccf5e
Josh Schneider <josh68@gmail.com>
2016-10-17 22:40:03 -0700

    fixed one test, at least

--- app/strings.js
+++ app/strings.js
@@ -1,22 +1,29 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
+  // reduceString: function(str, amount) {
+  //   const matches = {};
+  //   var newStr = str;
+  //   for (let i=0; i<str.length; i++) {
+  //     const char = str.charAt(i);
+  //     const re = new RegExp(char, 'g');
+  //     //matches[char] = matches[char] ? matches[char] : str.match(re);
+  //     matches[char] = matches[char] ? matches[char] : str.match(re);
+  //     if (matches[char].length > amount) {
+  //        console.log('matches for ' + char + ': ' + matches[char].length + ', amount to leave: ' + amount);
+  //        newStr = newStr.slice(0, newStr.lastIndexOf(char)) + newStr.slice(newStr.lastIndexOf(char) + 1);
+  //        matches[char].pop();
+  //     }
+  //   }
+  //   console.log(str);
+  //   return newStr;
+  // },
+
   reduceString: function(str, amount) {
-    const matches = {};
-    var newStr = str;
-    for (let i=0; i<str.length; i++) {
-      const char = str.charAt(i);
-      const re = new RegExp(char, 'g');
-      //matches[char] = matches[char] ? matches[char] : str.match(re);
-      matches[char] = matches[char] ? matches[char] : str.match(re);
-      if (matches[char].length > amount) {
-         console.log('matches for ' + char + ': ' + matches[char].length + ', amount to leave: ' + amount);
-         newStr = newStr.slice(0, newStr.lastIndexOf(char)) + newStr.slice(newStr.lastIndexOf(char) + 1);
-         matches[char].pop();
-      }
-    }
-    console.log(str);
-    return newStr;
+     const re = new RegExp('(.)\\1{' + amount + ',}', 'g');
+     //const matches = str.match(re);
+     //console.log(matches);
+     return str.replace(re, (match) =>  match.substring(0, amount));
   },
 
   wordWrap: function(str, cols) {
