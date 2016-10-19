7e3f61382d57333946e9f6bfabd410e2924ba266
Josh Schneider <josh68@gmail.com>
2016-10-18 20:59:15 -0700

    more bad code

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+    class MyClass {
+      constructor(x, y) {
+        this.greeting = x;
+        this.name = y;
+      }
+      sayIt() {
+        var greet = this.greeting + ', ' + this.name;
+        console.log(greet);
+        return greet;
+      }
+      set name(val) {
+        this.name = val;
+      }
+      set greeting(val) {
+        this.greeting = val;
+      }
+    }
+    return new MyClass(str1, str2);
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -1,40 +1,20 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
-  // reduceString: function(str, amount) {
-  //   const matches = {};
-  //   var newStr = str;
-  //   for (let i=0; i<str.length; i++) {
-  //     const char = str.charAt(i);
-  //     const re = new RegExp(char, 'g');
-  //     //matches[char] = matches[char] ? matches[char] : str.match(re);
-  //     matches[char] = matches[char] ? matches[char] : str.match(re);
-  //     if (matches[char].length > amount) {
-  //        console.log('matches for ' + char + ': ' + matches[char].length + ', amount to leave: ' + amount);
-  //        newStr = newStr.slice(0, newStr.lastIndexOf(char)) + newStr.slice(newStr.lastIndexOf(char) + 1);
-  //        matches[char].pop();
-  //     }
-  //   }
-  //   console.log(str);
-  //   return newStr;
-  // },
-
   reduceString: function(str, amount) {
      const re = new RegExp('(.)\\1{' + amount + ',}', 'g');
-     //const matches = str.match(re);
-     //console.log(matches);
      return str.replace(re, (match) =>  match.substring(0, amount));
   },
 
   wordWrap: function(str, cols) {
-    for (let i=4; i < str.length - 5; i+=5) {
-      //try splitting the string first - this isn't working yet
-      if ((/\s/).test(str.charAt(str[i]))) {
-        str.replace(/\s/, '\n');
-      } else {
-        str = str.substring(0, i) + '\n' + str.substring(i);
-      }
-    }
+    //broken
+    var strArr = str.split(' ');
+    var retStr = strArr.map((str) => {
+      return str.split('').map((char, idx) => {
+        return (/\s/).test(char) && idx % (cols - 1) === 0 ? '\n' : char;
+      }).join('');
+    }).map((str, idx, thisArr) => str.length === 1 ? str + ' ' : str + '\n').join('');
+    return retStr.substring(0, retStr.length - 1);
   },
 
   reverseString: function(str) {
